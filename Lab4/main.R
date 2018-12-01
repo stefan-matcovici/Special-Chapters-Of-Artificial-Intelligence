626381e6b4047a5d8238d5ff64996b5ad401c672
stefan-matcovici <stefan.matcovici@yahoo.com>
2018-12-01 21:40:03 +0200

    Further work on multiple regression

--- Lab4/main.R
+++ Lab4/main.R
@@ -1,38 +1,18 @@
 library(gtools)
 library(gtools)
 library(data.table)
+library(plyr)
 
 houses = read.table("house.dat",header=TRUE)
 
-#combinations = function(column_names) {
-#  n = length(column_names)
-#  result = array(0, n)
-#  all = 0
-#  
-#  combination_util(column_names, result, all, 1, n, 1)
-#}
-
-#combination_util = function(d, result, all, start, end, index) {
-#  if (index!=0) {
-#    if (result[index] == NA) {
-#      result[index] = c()
-#    }
-#  }
-#  
-#  if (index != length(d)) {
-#    i = start
-#    while(i <= end) {
-#      result[index] = d[i]
-#      combination_util(d, result, all, i + 1,  end, index + 1); 
-#      i = i+1
-#    }
-#
-#  }
-#}
-
-get_rss = function(samples, target, comb) {
+get_rss = function(samples, target, comb, s2) {
   no_combinations = nrow(comb)
-  result = matrix(nrow = no_combinations, ncol = 1)
+
+  p = ncol(comb)
+  m = nrow(samples)
+  
+  result = matrix(nrow = no_combinations, ncol = 4)
+  
   x = cbind(id=rep(1, 26), data.matrix(samples))
   y = data.matrix(target)
   for (i in c(1:no_combinations)) {
@@ -45,8 +25,14 @@ get_rss = function(samples, target, comb) {
     
     y_estimated = xx %*% beta
     rss = sum((y - y_estimated)**2)
+    tss = sum((y - mean(y))**2)
+    
+    r2 = 1 - rss/tss
     
     result[i, 1] = rss
+    result[i, 2] = r2
+    result[i, 3] = 1 - (1 - r2) * (m - 1) / (m - p - 1)
+    result[i, 4] = rss/s2 - (m - 2* (p + 1))
   }
   
   cbind(result, comb)
@@ -56,16 +42,34 @@ build_rss = function(d) {
   y = d["PRICE"]
   x = d[which(names(d) %in% "PRICE") * -1]
   
+  m = nrow(x)
+  n = length(d)
+  
   column_names = names(x)
   no_columns = length(column_names)
   
-  result = list()
+  rss_result = matrix(nrow = 0, ncol = no_columns+1)
+  r2_result = list()
+  ra_result = list()
+  cp_result = list()
+  
+  rss_total = get_rss(x, y, combinations(n=no_columns, r=no_columns, v=column_names), 1)
+  s_estimated = as.integer(rss_total[,1])/(m-n-1)
 
   for (i in c(1:no_columns)) {
-    rss = get_rss(x, y, combinations(n=no_columns, r=i, v=column_names))
-    result = append(result, list(rss[which.min(rss[,1]),]))
+    rss = get_rss(x, y, combinations(n=no_columns, r=i, v=column_names), s_estimated)
+    
+    best_rss = which.min(rss[,1])
+    best_r2 = which.max(rss[,2])
+    best_ra = which.max(rss[,3])
+    best_cp = which.min(abs(as.integer(rss[,4]) - i))
+    
+    rss_result = rbind.fill.matrix(rss_result, rss[best_rss, c(1, 5:(5+i-1))])
+    r2_result = append(r2_result, list(rss[best_r2, c(2, 5:(5+i-1))]))
+    ra_result = append(ra_result, list(rss[best_ra, c(3,5:(5+i-1))]))
+    cp_result = append(cp_result, list(rss[best_cp, c(4,5:(5+i-1))]))
   }
-  result
+  cp_result
 }
 
 l = build_rss(houses)
@@ -81,4 +85,8 @@ colnames(df) = c("rss", c(1:13))
 #ics = houses[which(names(houses) %in% "PRICE") * -1]
 #get_rss(ics, igrec, combinations(n=length(names(x)), r=2, v=names(x)))
 
-#lm(houses$PRICE ~ houses$BRD + houses$BTH)
+l = lm(houses$PRICE ~ houses$BRD+houses$BTH+houses$CDN+houses$CON+houses$FLR+houses$FP+houses$GAR+houses$L1+houses$L2+houses$LOT+houses$RMS+houses$ST+houses$TAX)
+l = lm(houses$PRICE ~ houses$L2)
+sse = sum(l$residuals^2)
+s2 = sse / (26-13)
+sse/s2 - (26 -2*13)
