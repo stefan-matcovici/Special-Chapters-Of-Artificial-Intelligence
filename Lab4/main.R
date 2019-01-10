b76c222b21cd7cdef31dda35c9d57c7a8c95de41
stefan-matcovici <stefan.matcovici@yahoo.com>
2019-01-10 23:19:59 +0200

    Finished work on multiple regression

--- Lab4/main.R
+++ Lab4/main.R
@@ -2,6 +2,8 @@ library(gtools)
 library(gtools)
 library(data.table)
 library(plyr)
+library(olsrr)
+
 
 houses = read.table("house.dat",header=TRUE)
 
@@ -32,7 +34,7 @@ get_rss = function(samples, target, comb, s2) {
     result[i, 1] = rss
     result[i, 2] = r2
     result[i, 3] = 1 - (1 - r2) * (m - 1) / (m - p - 1)
-    result[i, 4] = rss/s2 - (m - 2* (p + 1))
+    result[i, 4] = rss/s2 - (m - 2* p - 2)
   }
   
   cbind(result, comb)
@@ -48,44 +50,92 @@ build_rss = function(d) {
   column_names = names(x)
   no_columns = length(column_names)
   
-  rss_result = matrix(nrow = 0, ncol = no_columns+1)
+  rss_result = list()
   r2_result = list()
   ra_result = list()
   cp_result = list()
   
-  rss_total = get_rss(x, y, combinations(n=no_columns, r=no_columns, v=column_names), 1)
-  s_estimated = as.integer(rss_total[,1])/(m-n-1)
+  rss_total = get_rss(x, y, t(combn(x=column_names, m=no_columns)), 1)
+  s_estimated = as.integer(rss_total[,1])/(m-no_columns-1)
 
-  for (i in c(1:no_columns)) {
-    rss = get_rss(x, y, combinations(n=no_columns, r=i, v=column_names), s_estimated)
+  for (i in c(0:no_columns)) {
+    rss = get_rss(x, y, t(combn(x=column_names, m=i)), s_estimated)
     
     best_rss = which.min(rss[,1])
     best_r2 = which.max(rss[,2])
     best_ra = which.max(rss[,3])
-    best_cp = which.min(abs(as.integer(rss[,4]) - i))
+    best_cp = which.min(abs(as.integer(rss[,4]) - i - 1))
     
-    rss_result = rbind.fill.matrix(rss_result, rss[best_rss, c(1, 5:(5+i-1))])
-    r2_result = append(r2_result, list(rss[best_r2, c(2, 5:(5+i-1))]))
-    ra_result = append(ra_result, list(rss[best_ra, c(3,5:(5+i-1))]))
-    cp_result = append(cp_result, list(rss[best_cp, c(4,5:(5+i-1))]))
+    if (i==0) {
+      rss_result = append(rss_result, list(rss[best_rss, 1]))
+      r2_result = append(r2_result, list(rss[best_r2, 2]))
+      ra_result = append(ra_result, list(rss[best_ra, 3]))
+      cp_result = append(cp_result, list(rss[best_cp, 4]))
+    }
+    else {
+      rss_result = append(rss_result, list(rss[best_rss, c(1, 5:(5+i-1))]))
+      r2_result = append(r2_result, list(rss[best_r2, c(2, 5:(5+i-1))]))
+      ra_result = append(ra_result, list(rss[best_ra, c(3,5:(5+i-1))]))
+      cp_result = append(cp_result, list(rss[best_cp, c(4,5:(5+i-1))]))
+    }
   }
-  cp_result
+  list(to_dataframe('rss', rss_result), to_dataframe("r2", r2_result), to_dataframe("ra", ra_result), to_dataframe("cp", cp_result))
 }
 
-l = build_rss(houses)
-mx <- max(lengths(l))
-l1 = lapply(l, `length<-`, mx)
+to_dataframe = function(name, data) {
+  mx <- max(lengths(data))
+  l1 = lapply(data, `length<-`, mx)
+  
+  df <- transpose(as.data.frame(l1))
+  colnames(df) = c(name, c(1:13))
+  
+  df
+}
 
-df <- t(as.data.frame(l1))
-df <- transpose(as.data.frame(l1))
-colnames(df) = c("rss", c(1:13))
+result = build_rss(houses)
 
+png(file = "png/rss-criterium.png")
+plot(x=0:13, transpose(result[[1]]['rss']), type="o",  main="Plot for rss criterium ", xlab="no. variables", ylab="value", pch=16, xlim=c(0,13), ylim=c(0, 4000))
+dev.off()
+png(file = "png/r-squared-criterium.png")
+plot(x=0:13, transpose(result[[2]]['r2']), type="o",  main="Plot for r-square criterium ", xlab="no. variables", ylab="value", pch=16, xlim=c(0,13), ylim=c(0, 1))
+dev.off()
+png(file = "png/r-adjusted-criterium.png")
+plot(x=0:13, transpose(result[[3]]['ra']), type="o",  main="Plot for r adjusted criterium ", xlab="no. variables", ylab="value", pch=16, xlim=c(0,13), ylim=c(0, 1))
+dev.off()
+png(file = "png/cp-criterium.png")
+plot(x=0:13, transpose(result[[4]]['cp']), type="o",  main="Plot for cp criterium ", xlab="no. variables", ylab="value", pch=16, col="blue", xlim=c(0,13), ylim = c(0, 165))
+lines(c(0:13), type = "o", pch=10, col=26)
+legend("topright", legend=c("cp", "identity function"), col=c("red", "blue"), pch=c(16, 10), cex=0.8)
+dev.off()
+
+r_threshold = 0.88
+cp_threshold = 1
+for (i in c(4:8)) { 
+  if (abs(as.numeric(result[[4]]['cp'][i,]) - i - 1) < cp_threshold && as.numeric(result[[3]]['ra'][i,]) > r_threshold) {
+    print(sprintf("best model with cp: %s and ra: %s ", result[[4]]['cp'][i,], result[[3]]['ra'][i,]))
+    print(result[[4]][i,2:i])
+  }
+}
+
+last_cp = as.numeric(result[[4]]['cp'][1,])
+i = 1
+while (last_cp > i + 1) {
+  last_cp = as.numeric(result[[4]]['cp'][i,])
+  i = i+1
+}
+print(i-1)
+
+get_rss(x, )
 
 #igrec = houses["PRICE"]
 #ics = houses[which(names(houses) %in% "PRICE") * -1]
 #get_rss(ics, igrec, combinations(n=length(names(x)), r=2, v=names(x)))
 
-l = lm(houses$PRICE ~ houses$BRD+houses$BTH+houses$CDN+houses$CON+houses$FLR+houses$FP+houses$GAR+houses$L1+houses$L2+houses$LOT+houses$RMS+houses$ST+houses$TAX)
+full_model = lm(houses$PRICE ~ houses$BRD+houses$BTH+houses$CDN+houses$CON+houses$FLR+houses$FP+houses$GAR+houses$L1+houses$L2+houses$LOT+houses$RMS+houses$ST+houses$TAX)
+l = lm(houses$PRICE ~ houses$BRD+houses$BTH+houses$CDN+houses$CON+houses$FLR+houses$FP+houses$GAR+houses$L1+houses$L2+houses$LOT+houses$RMS+houses$ST)
+ols_mallows_cp(l, full_model)
+summary(l)
 l = lm(houses$PRICE ~ houses$L2)
 sse = sum(l$residuals^2)
 s2 = sse / (26-13)
